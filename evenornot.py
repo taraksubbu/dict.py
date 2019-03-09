15ff9f75a2ecdd030c9e7c6ed031a95d1418f5ea
taraksubbu <48044738+taraksubbu@users.noreply.github.com>
2019-03-09 12:03:52 +0530

    Add files via upload

--- evenornot.py
+++ evenornot.py
@@ -1,6 +1,5 @@
-    a=int(input("Please Enter a Number : "));  
-    if(a%2==0):  
-        print("This Number is Even")  
-    else:  
-        print("This Number is Odd")  
-
+a=int(input("Please Enter a Number : "));  
+if(a%2==0):  
+    print("This Number is Even")  
+else:  
+	print("This Number is Odd")  
